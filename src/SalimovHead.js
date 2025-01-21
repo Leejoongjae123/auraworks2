1a3512f30230ac1ee5bd7028a6b807e0d84a4348
Leejoongjae123 <ljj3347@naver.com>
2025-01-21 13:56:04 +0900

    '123'

--- src/SalimovHead.js
+++ src/SalimovHead.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 const SalimovHead = () => {
   return (
     <Head>
-      <title>AURAWORKS</title>
+      <title>아우라웍스|AURAWORKS</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Template Google Fonts */}
